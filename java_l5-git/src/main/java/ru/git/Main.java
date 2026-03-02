8143060f4a630b32dfe07341788353cf15c03f75
123 <i.v.matveev@yandex.ru>
2026-03-02 16:16:07 +0300

    новое в приветствии 2

--- java_l5-git/src/main/java/ru/git/Main.java
+++ java_l5-git/src/main/java/ru/git/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, 1111!");
+        System.out.println("Hello, 22222!");
     }
 }
