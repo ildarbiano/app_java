0b23019028d0b3f2b30de200099ccf5aa9b296b1
123 <i.v.matveev@yandex.ru>
2026-03-02 16:52:06 +0300

    очень важные правки в ветке feach_1

--- java_l5-git/src/main/java/ru/git/Main.java
+++ java_l5-git/src/main/java/ru/git/Main.java
@@ -3,8 +3,9 @@ package ru.git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, 22222!");
+        System.out.println("Hello, 221212121!");
         System.out.println("Hello, 3!");
         int a = 10;
+        System.out.println( "a =" + a);
     }
 }
