60913b05f7a5446382e2844d34fa0329c4c2f7bf
123 <il.v.matveev@yandex.ru>
2026-03-02 15:44:04 +0300

    новое в приветствии

---
+++ java_l5-git/src/main/java/ru/git/Main.java
@@ -0,0 +1,8 @@
+package ru.git;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Ilya!");
+    }
+}
