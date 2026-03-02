1d02e607fffe756df1db17d8533b03b5641f61d1
123 <i.v.matveev@yandex.ru>
2026-03-02 16:20:00 +0300

    новое в приветствии 3 и удаление 111

--- java_l5-git/src/main/java/ru/git/Main.java
+++ java_l5-git/src/main/java/ru/git/Main.java
@@ -3,7 +3,7 @@ package ru.git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, 1111!");
         System.out.println("Hello, 22222!");
+        System.out.println("Hello, 3!");
     }
 }
